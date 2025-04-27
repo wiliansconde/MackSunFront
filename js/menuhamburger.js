3789458272053f7b21ab09f422f8b34570b44339
Felipe Gonzalez <felipecgonzalez2305@gmail.com>
2025-04-27 16:31:29 -0300

    refactor: funcionamento do menu hamburguer

--- js/menuhamburger.js
+++ js/menuhamburger.js
@@ -1,8 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const menuHamburger = document.getElementById('menu_hamburger');
-    const menu = document.querySelector('.menu');
+    loadHTML("header", "header.html", () => {
+        const menuHamburger = document.getElementById('menu_hamburger');
+        const menu = document.querySelector('.menu');
 
-    menuHamburger.addEventListener('click', () => {
-        menu.classList.toggle('active');
+        menuHamburger.addEventListener('click', () => {
+            menu.classList.toggle('active');
+        })
     });
 });
+
+function loadHTML(id, file, callback) {
+    fetch(file)
+        .then((response) => {
+            if (!response.ok) throw new Error(`Erro ao carregar ${file}`);
+            return response.text();
+        })
+        .then((data) => {
+            document.getElementById(id).innerHTML = data;
+            if (callback) callback();
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
