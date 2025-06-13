82a18813fc9f6565344883523a4e474caa527eec
Felipe Gonzalez <felipecgonzalez2305@gmail.com>
2025-06-13 11:40:14 -0300

    fix: Correção nas mensagens de sucesso e erro que estão sendo exibidas na mesma tela.

--- js/requisicaosenha.js
+++ js/requisicaosenha.js
@@ -6,10 +6,13 @@ function recuperarSenha() {
     const btnRecuperar = document.getElementById('formulario_btn')
     const email_valido = document.getElementById('email_valido')
     const msgErro = document.querySelector('.invalid_message_error')
-    const msgSucesso = document.querySelector('.success-message')
+    const msgSucesso = document.querySelector('.valid_message_error')
 
     btnRecuperar.addEventListener('click', async (event) => {
         event.preventDefault();
+
+        msgErro.style.display = 'none';
+        msgSucesso.style.display = 'none';
 
         const email = email_valido.value.trim();
 
